483b01c55110c800ec568b67080c0ae357979753
elchinma <elchinma@code.edu.az>
2022-11-27 00:34:22 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool t = true;
+            string s;
+            if (t)
+            {
+                s = "Bu bir custom contains methodudur";
+            }
+       
+            else
+            {
+                s = "Bu bir custom contains methodudur";
+            }
+            Console.WriteLine(s);
+
+        }
+    }
+}
